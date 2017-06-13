b2d0f758ca8828e5eed9341bab96d82ead9fcb5b
shenshuai89 <shenshuai89@qq.com>
2017-06-13 11:18:53 +0800

    初步启动环境成功运行

---
+++ gulpfile.js
@@ -0,0 +1,85 @@
+/**
+ * Created by sam on 2017/6/13.
+ */
+var gulp = require('gulp')
+var $ = require('gulp-load-plugins')()
+var open = require('open')
+
+var app = {
+    srcPath:'src/',
+    devPath:'build/',
+    prdPath:'dev/'
+}
+
+gulp.task('lib', function () {
+    gulp.src('bower_components/**/*.js')
+        .pipe(gulp.dest(app.devPath + 'vendor'))
+        .pipe(gulp.dest(app.prdPath + 'vendor'))
+        .pipe($.connect.reload())
+})
+
+gulp.task('html', function () {
+    gulp.src(app.srcPath+ '**/*.html')
+        .pipe(gulp.dest(app.devPath))
+        .pipe(gulp.dest(app.prdPath))
+        .pipe($.connect.reload())
+})
+
+gulp.task('json', function () {
+    gulp.src(app.srcPath+'data/**/*.json')
+        .pipe(gulp.dest(app.devPath + 'data'))
+        .pipe(gulp.dest(app.prdPath + 'data'))
+        .pipe($.connect.reload())
+})
+
+gulp.task('less', function () {
+    gulp.src(app.srcPath + 'style/index.less')
+        .pipe($.less())
+        .pipe(gulp.dest(app.devPath + 'css'))
+        .pipe($.cssmin())
+        .pipe(gulp.dest(app.prdPath + 'css'))
+        .pipe($.connect.reload())
+})
+
+gulp.task('js', function () {
+    gulp.src(app.srcPath +'script/**/*.js')
+        .pipe($.concat('index.js'))
+        .pipe(gulp.dest(app.devPath +'js'))
+        .pipe($.uglify())
+        .pipe(gulp.dest(app.prdPath +'js'))
+        .pipe($.connect.reload())
+})
+
+gulp.task('image', function () {
+    gulp.src(app.srcPath +'image/**/*')
+        .pipe(gulp.dest(app.devPath+'image'))
+        .pipe($.imagemin())
+        .pipe(gulp.dest(app.prdPath+'image'))
+        .pipe($.connect.reload())
+})
+
+gulp.task('build', ['lib', 'html', 'less', 'json', 'js', 'image'])
+
+gulp.task('clean', function () {
+    gulp.src([app.devPath,app.prdPath])
+        .pipe($.clean())
+})
+
+//启动一个服务器
+gulp.task('serve',['build'], function () {
+    $.connect.server({
+        root:[app.devPath],
+        livereload:true,
+        port:1111
+    })
+    open('http://localhost:1111')
+
+    gulp.watch('bower_components/**/*',['lib'])
+    gulp.watch(app.srcPath + '**/*.html', ['html'])
+    gulp.watch(app.srcPath + 'data/**/*.json', ['json'])
+    gulp.watch(app.srcPath + 'style/**/*.less', ['less'])
+    gulp.watch(app.srcPath + 'script/**/*.js',['js'])
+    gulp.watch(app.srcPath + 'image/**/*',['image'])
+})
+
+gulp.task('default', ['serve'])
